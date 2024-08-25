bbd26be15c1d3086520c74f269d6f8c187d02997
Abstract-ak <ash.king0205@gmail.com>
2024-08-25 13:43:59 +0530

    quiz complete logic and shuffle the answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     //this way is defined that we cannot lose the prevState and update the new state inside the array.
@@ -13,12 +16,28 @@ export default function Quiz() {
     });
   }
 
+  //Quiz is completed renders--
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Img" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  //Shuffle the array answers with random numbers
+  /* Math.random() - 0.5 : probability 50/100 that is +ve/-ve
+   */
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
